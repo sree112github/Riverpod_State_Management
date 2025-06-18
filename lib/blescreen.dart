af2ac23ea3a10fdb314190808e43bd3dcc4be850
SREEKUTTAN J S <85542393+sree112github@users.noreply.github.com>
2025-06-18 23:32:41 +0530

    Update blescreen.dart

--- lib/blescreen.dart
+++ lib/blescreen.dart
@@ -1,8 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'bleDI.dart';
-
 
 class BluetoothScreen extends ConsumerWidget {
   final String deviceId;
@@ -12,8 +13,7 @@ class BluetoothScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final controller = ref.read(bluetoothControllerProvider.notifier);
-    final state = ref.watch(bluetoothControllerProvider);
-
+    log("Entire widget is rebuilding");
 
     return Scaffold(
       appBar: AppBar(
@@ -22,26 +22,52 @@ class BluetoothScreen extends ConsumerWidget {
           IconButton(
             icon: Icon(Icons.link_off),
             onPressed: () => controller.disconnect(),
-          )
+          ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            Consumer(
+              builder: (context, ref, child) {
+                log("COnnection widget rebuild");
+                final isConnected = ref.watch(bluetoothControllerProvider.select((state)=>state.isConnected));
+                return isConnected?Text(
+                  "Connection status: ${isConnected
+                      ? "Connected"
+                      : "Disconnected"}",
+                ): CircularProgressIndicator();
+              }),
 
-            Text("Connection status: ${state.isConnected ? "Connected" : "Disconnected"}"),
             const SizedBox(height: 16),
-            if (state.receivedData != null)
-              Text("Received: ${state.receivedData}"),
+            Consumer(
+              builder: (context,ref,child) {
+                log("recieved data part rebuild");
+                final isSending = ref.watch(bluetoothControllerProvider.select((state)=>state.isSending));
+                final receivedData = ref.watch(bluetoothControllerProvider.select((state)=> state.receivedData));
+                 if(receivedData != null) {
+                   return isSending ? CircularProgressIndicator() : Text(
+                       "Recieved Data: ${receivedData}");
+                 }else{
+                   return Text("Data is not found");
+                 }
+                 
+              }),
 
-            if(state.isSending)...[
-              CircularProgressIndicator()
-            ],
             const Spacer(),
-            ElevatedButton(
-              onPressed: state.isConnected ? () => controller.sendData("scanNetworks") : null,
-              child: const Text("Send Hello"),
+            Consumer(
+              builder: (context, ref, child) {
+                log("send hello button rebuild");
+                final state = ref.watch(bluetoothControllerProvider);
+
+                return ElevatedButton(
+                  onPressed: state.isConnected
+                      ? () => controller.sendData("scanNetworks")
+                      : null,
+                  child: const Text("Send Hello"),
+                );
+              },
             ),
             ElevatedButton(
               onPressed: () => controller.connect(deviceId),
